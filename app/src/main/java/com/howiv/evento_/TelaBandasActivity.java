f5c4174a79dcf0527ec1c4711503ca2efa3558b0
Romulo Goelzer Portolann <romulogoelzer@gmail.com>
2022-07-21 03:08:09 -0300

    TelaBandasActivity

--- app/src/main/java/com/howiv/evento_/TelaBandasActivity.java
+++ app/src/main/java/com/howiv/evento_/TelaBandasActivity.java
@@ -2,13 +2,27 @@ package com.howiv.evento_;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.howiv.evento_.model.Banda;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TelaBandasActivity extends AppCompatActivity {
+
+    private List<Banda> bandas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_bandas);
     }
+
+    public void abrirTelaAddBanda(View view) {
+//        startActivity(new Intent(this, TelaAdicionarBandasActivity.class));
+    }
+
 }
