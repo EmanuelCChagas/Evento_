ae7d3d1d33bb0e377f44bc7b26cec112b5d7a49f
Emanuel <emanuelcarturano21@gmail.com>
2022-07-20 21:55:53 -0300

    commit menu principal

--- app/src/main/java/com/howiv/evento_/MainActivity.java
+++ app/src/main/java/com/howiv/evento_/MainActivity.java
@@ -1,8 +1,13 @@
 package com.howiv.evento_;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,4 +24,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_principal,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.itemEventos:
+                this.abrirEventos();
+                break;
+            case  R.id.itemArtistas:
+                this.abrirArtistas();
+                break;
+            case R.id.itemBandas:
+                this.abrirBandas();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public final void abrirEventos() {
+    }
+
+    public final void abrirArtistas() {
+       // this.startActivity(new Intent((Context)this, TelaArtistasActivity.class));
+    }
+
+    public final void abrirBandas() {
+        //this.startActivity(new Intent((Context)this, TelaBandasActivity.class));
+    }
+
 }
